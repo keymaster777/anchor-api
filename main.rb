4e04f98628995f3413a97aba2ccd9752cb158c88
Riley Cleary <rileykeycleary@yahoo.com>
2022-08-01 22:34:59 -0600

    Adds multiplication endpoint and ensures an appropriate response and safety with input data

--- main.rb
+++ main.rb
@@ -15,4 +15,18 @@ class AnchorApi < Sinatra::Base
   get '/' do
     json status: 200, message: 'Hello world.'
   end
+
+  post '/multiply5' do
+    if ( params.key?("value") && is_numeric?(params["value"]))
+      json status: 200, message: (params["value"].to_f * 5) #Casting value to float so that result isnt unintentionally floored
+    else
+      json status: 400, message: "Invalid value given."
+    end
+  end
+
+  private
+
+  def is_numeric? (val)
+    val == "#{val.to_f}" || val == "#{val.to_i}" 
+  end
 end
